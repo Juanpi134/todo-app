5f14d2cac30e34c98f1767b7d060a2fa7a1801dc
Juanpi134 <juanpitocino_15@hotmail.com>
2026-07-04 13:28:29 -0300

    se crea el nuevo estado para añadir una tarea al array de tareas y se imprime por consola el valor actual

--- src/componentes/TodoList.jsx
+++ src/componentes/TodoList.jsx
@@ -6,8 +6,15 @@ function ToDoList(){
     //se crean los estados de las tareas
     const [tasks,setTasks] = useState([]);
 
-    
+    //el valor inicial es un arreglo vacío
     console.log(tasks);
+
+    //se crea un nuevo estado para añadir la tarea
+    //esto se añadirá al array de tasks
+    const [newTask,setNewTask] = useState("");
+
+    //se muestra por pantalla y me muestra un array vacío
+    console.log(newTask)
 }
 
 //se exporta el componente
