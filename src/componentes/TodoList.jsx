f4ce10a4a133849edc9b247dc7dc5e822451cdfc
Juanpi134 <juanpitocino_15@hotmail.com>
2026-07-04 13:25:35 -0300

    prueba de que me devuelve el arreglo tasks

--- src/componentes/TodoList.jsx
+++ src/componentes/TodoList.jsx
@@ -3,7 +3,11 @@ import { useState } from "react";
 
 //creacion del componente todoList
 function ToDoList(){
+    //se crean los estados de las tareas
+    const [tasks,setTasks] = useState([]);
 
+    
+    console.log(tasks);
 }
 
 //se exporta el componente
