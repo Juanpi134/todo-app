14d45a35aefc6ab9596fd80949ec0cc324a686ee
Juanpi134 <juanpitocino_15@hotmail.com>
2026-07-04 14:49:30 -0300

    se añade por cada tarea un boton

--- src/componentes/TodoList.jsx
+++ src/componentes/TodoList.jsx
@@ -2,74 +2,74 @@
 import { useState } from "react";
 
 //creacion del componente todoList
-function ToDoList(){
-    //se crean los estados de las tareas
-    const [tasks,setTasks] = useState(["hola","chau"]);
+function ToDoList() {
+  //se crean los estados de las tareas
+  const [tasks, setTasks] = useState(["hola", "chau"]);
 
-    //el valor inicial es un arreglo vacío
-    //console.log(tasks);
+  //el valor inicial es un arreglo vacío
+  //console.log(tasks);
 
-    //se crea un nuevo estado para añadir la tarea
-    //esto se añadirá al array de tasks
-    const [newTask,setNewTask] = useState("");
+  //se crea un nuevo estado para añadir la tarea
+  //esto se añadirá al array de tasks
+  const [newTask, setNewTask] = useState("");
 
-    //se muestra por pantalla y me muestra un array vacío
-    //console.log(newTask)
+  //se muestra por pantalla y me muestra un array vacío
+  //console.log(newTask)
 
-    //se crea la función manejador de inputs 
-    //cada vez que hago un cambio como escribir me devuelve el objeto SyntheticBaseEvent
-    const manejadorDeCambiosEnElInput = (evento) => {
-        //el evento target me devuelve el elemento HTML
-        //console.log(evento.target);
+  //se crea la función manejador de inputs
+  //cada vez que hago un cambio como escribir me devuelve el objeto SyntheticBaseEvent
+  const manejadorDeCambiosEnElInput = (evento) => {
+    //el evento target me devuelve el elemento HTML
+    //console.log(evento.target);
 
-        //cuando imprimo me devuelve undefined 
-        if(setNewTask(evento.target.value) === "undefined"){
-            console.log("hola");
-        }
-        
+    //cuando imprimo me devuelve undefined
+    if (setNewTask(evento.target.value) === "undefined") {
+      console.log("hola");
     }
-    //esta funcion lo que hace es agregar una tarea al array de tareas
-    const addTask = () => {
+  };
+  //esta funcion lo que hace es agregar una tarea al array de tareas
+  const addTask = () => {};
 
-    }
+  //se crea la funcion eliminar tarea que recibe un índice para buscar y eliminar
+  const deleteTask = (indice) => {};
 
-    //se crea la funcion eliminar tarea que recibe un índice para buscar y eliminar
-    const deleteTask = (indice) => {
+  //se crea la funcion moveTaskUp
+  //recibe un indice para mover el indice de la lista adelante
+  const moveTaskUp = (indice) => {};
 
-    }
+  //se crea la funcion moveTaskDown
+  //recibe un indice de la lista para moverla hacia abajo
+  const moveTaskDown = (indice) => {};
 
-    //se crea la funcion moveTaskUp 
-    //recibe un indice para mover el indice de la lista adelante
-    const moveTaskUp = (indice) => {
+  return (
+    <div className="to-do-list">
+      <h1>To-Do-List</h1>
 
-    }
+      <div>
+        <input
+          type="text"
+          placeholder="Escribe una tarea"
+          value={newTask}
+          name=""
+          id=""
+          onChange={manejadorDeCambiosEnElInput}
+        />
+        <button className="botonAgregar" onClick={addTask}>
+          Agregar
+        </button>
+      </div>
 
-    //se crea la funcion moveTaskDown
-    //recibe un indice de la lista para moverla hacia abajo
-     const moveTaskDown = (indice) => {
-
-    }
-    
-    return (
-        <div className="to-do-list">
-            <h1>To-Do-List</h1>
-
-            <div>
-            <input type="text" placeholder="Escribe una tarea" value={newTask} name="" id="" onChange={manejadorDeCambiosEnElInput}/>
-            <button className="botonAgregar" onClick={addTask}>Agregar</button>
-            </div>
-
-            <ol>
-                {/* prueba de ver como se renderiza en consola los elementos del array actual */}
-                {/* cada tarea recibe un elemento a renderizar */}
-                {/*  
+      <ol>
+        {/* prueba de ver como se renderiza en consola los elementos del array actual */}
+        {/* cada tarea recibe un elemento a renderizar */}
+        {/*  
                 {tasks.map((elemento) => {
                     console.log(elemento)
                 })}
                     */}
-                    {/* react añade una key para cada elemento */}
-                    {/* cada elemento recibe un índice y el tipo de elemento "li" */}
-                    {/*  
+        {/* react añade una key para cada elemento */}
+        {/* cada elemento recibe un índice y el tipo de elemento "li" */}
+        {/*  
                 {
                     tasks.map((tarea,indice) => {
                         console.log(<li key={indice}></li>)
@@ -78,15 +78,18 @@ function ToDoList(){
                 }
                     */}
 
-                    {tasks.map((tarea,indice) => {
-                        // creamos la tarea y le asignamos el indice a cada li
-                        <li key="indice">
-                            <span className="text">{task}</span>
-                        </li>
-                    })}
-            </ol>
-        </div>
-    )
+        {tasks.map((tarea, indice) => (
+          <li key={indice}>
+            <span className="text">{tarea}</span>
+            {/* por cada tarea creamos un boton  */}
+            <button>
+                Delete
+            </button>
+          </li>
+        ))}
+      </ol>
+    </div>
+  );
 }
 
 //se exporta el componente
