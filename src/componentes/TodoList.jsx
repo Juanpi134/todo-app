0689503d79c0503241ce395d8e5504f006a7507c
Juanpi134 <juanpitocino_15@hotmail.com>
2026-07-04 13:52:03 -0300

    imprimo para probar valores de mi setNewTask y paso el evento con su valor y target

--- src/componentes/TodoList.jsx
+++ src/componentes/TodoList.jsx
@@ -7,20 +7,26 @@ function ToDoList(){
     const [tasks,setTasks] = useState([]);
 
     //el valor inicial es un arreglo vacío
-    console.log(tasks);
+    //console.log(tasks);
 
     //se crea un nuevo estado para añadir la tarea
     //esto se añadirá al array de tasks
     const [newTask,setNewTask] = useState("");
 
     //se muestra por pantalla y me muestra un array vacío
-    console.log(newTask)
+    //console.log(newTask)
 
     //se crea la función manejador de inputs 
     //cada vez que hago un cambio como escribir me devuelve el objeto SyntheticBaseEvent
     const manejadorDeCambiosEnElInput = (evento) => {
         //el evento target me devuelve el elemento HTML
         //console.log(evento.target);
+
+        //cuando imprimo me devuelve undefined 
+        if(setNewTask(evento.target.value) === "undefined"){
+            console.log("hola");
+        }
+        
     }
     //esta funcion lo que hace es agregar una tarea al array de tareas
     const addTask = () => {
@@ -45,8 +51,12 @@ function ToDoList(){
     }
     
     return (
-        <div>
-            <input type="text" name="" id="" onChange={manejadorDeCambiosEnElInput}/>
+        <div className="to-do-list">
+            <h1>To-Do-List</h1>
+
+            <div>
+            <input type="text" placeholder="Escribe una tarea" value={newTask} name="" id="" onChange={manejadorDeCambiosEnElInput}/>
+            </div>
         </div>
     )
 }
