96067231347394b514d11ec6644b36ce504d7acc
Juanpi134 <juanpitocino_15@hotmail.com>
2026-07-04 13:20:19 -0300

    se importa el hook useState para luego realizar tareas que cambien el estado

---
+++ src/componentes/TodoList.jsx
@@ -0,0 +1,10 @@
+//se importa el hook useState
+import { useState } from "react";
+
+//creacion del componente todoList
+function ToDoList(){
+
+}
+
+//se exporta el componente
+export default ToDoList;
